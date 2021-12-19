66b177f3c8b0e2626ee274ea9785f6e30ac4e874
Cristina Eiras Rodríguez <ceiras@uoc.edu>
2021-12-19 20:05:58 +0100

    Adds select image from gallery

--- app/src/main/java/com/dam/puzzlepals/ui/SelectImgActivity.java
+++ app/src/main/java/com/dam/puzzlepals/ui/SelectImgActivity.java
@@ -2,11 +2,19 @@ package com.dam.puzzlepals.ui;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dam.puzzlepals.R;
@@ -14,22 +22,57 @@ import com.dam.puzzlepals.holders.PuzzleHolder;
 
 public class SelectImgActivity extends AppCompatActivity {
 
+    private boolean imageSelected = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_img);
 
         ImageView selectedImageView = findViewById(R.id.selected_img_view);
-        selectedImageView.setImageResource(R.drawable.puzzle_pals_logo);
+        ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                TextView alertText = findViewById(R.id.alert_text);
+                Button nextButton = findViewById(R.id.next_button);
+                if (result.getResultCode() == RESULT_OK) {
+                    SelectImgActivity.this.imageSelected = true;
+                    nextButton.setClickable(SelectImgActivity.this.imageSelected);
+                    alertText.setVisibility(View.INVISIBLE);
+                    Uri photoPath = result.getData().getData();
+                    selectedImageView.setImageURI(photoPath);
+                    BitmapDrawable drawable = (BitmapDrawable) selectedImageView.getDrawable();
+                    Bitmap bitmap = drawable.getBitmap();
+                    PuzzleHolder.getInstance().getPuzzle().setImage(bitmap);
+                } else {
+                    nextButton.setClickable(SelectImgActivity.this.imageSelected);
+                    nextButton.setBackgroundColor(Color.GRAY);
+                    alertText.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
-        BitmapDrawable drawable = (BitmapDrawable) selectedImageView.getDrawable();
-        Bitmap bitmap = drawable.getBitmap();
-        PuzzleHolder.getInstance().getPuzzle().setImage(bitmap);
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+
+        Button selectImageButton = findViewById(R.id.select_img_button);
+        selectImageButton.setOnClickListener(view -> launcher.launch(intent));
+        selectedImageView.setOnClickListener(view -> launcher.launch(intent));
+
+        launcher.launch(intent);
+    }
+
+    public void loadImageFromGallery(View view) {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivity(galleryIntent);
     }
 
     public void chooseLevel(View view) {
-        Intent selectLevelActivityIntent = new Intent(this, SelectLevelActivity.class);
-        startActivity(selectLevelActivityIntent);
+        if (this.imageSelected) {
+            Intent selectLevelActivityIntent = new Intent(this, SelectLevelActivity.class);
+            startActivity(selectLevelActivityIntent);
+        }
     }
 
 }
